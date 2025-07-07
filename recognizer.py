a14af85f80fd6ed553795b2c90459c2276fbb873
Faizul <faizulmd1306@gmail.com>
2025-07-07 13:00:46 +0530

    First commit

---
+++ recognizer.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+def audio_recognizer():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as Source:
+        print("Listening...")
+        audio = r.listen(Source)
+
+    try:
+        command = r.recognize_google(audio, language="en-IN")
+    except sr.UnknownValueError:
+        print("Could not understand audio.")
+    except sr.RequestError as e:
+        print("Could not request results; check your internet connection.")
+
+    return command
+
+# text = audio_recognizer()
+# print(text)
